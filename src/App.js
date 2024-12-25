addcec82252a746ada81322f73d6f2d21994d771
SyedSaqhibSuheel <syedsaqhibsuheel@gmail.com>
2024-12-25 20:42:42 +0530

    intial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import pic from './pic.jpg';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+   <div className = "Protfolio">
+        <div className="wrapper">
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+     </div>
+    <div>
+      <img src={pic} alt="img" className="photo"/>
+    <h1>Hi all, I'm Syed Saqhib Suheel <span>👋</span></h1>
+    <h3 className="info">A passionate Frontend Software Developer 🚀 having <br/> an experience of building Web applications with <br/> JavaScript / Reactjs / Material UI  and some <br/> other cool libraries and frameworks.</h3>
+    <div className='icons'>
+      <GitHubIcon/>
+      <LinkedInIcon/>
+      <InstagramIcon/>
+      <EmailIcon/>
+      
+
     </div>
+
+    <button className='button'>Contract Me</button>
+    <button className='button'>Resume Download</button>
+    </div>
+  
+
+
+ 
+     
+   </div>
   );
 }
 
